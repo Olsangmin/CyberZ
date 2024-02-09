b844de1bbd5a6a89d4c4abf2062ced977d94bdc9
Sang Min <alstkdalsdl@naver.com>
2024-02-09 12:15:12 +0900

    Check use network

--- Client/stdafx.h
+++ Client/stdafx.h
@@ -46,7 +46,7 @@ using namespace std;
 #include "Common.h"
 #include "Network.h"
 
-#define USE_NETWORK // 주석하면 서버없이 가능@@@@@@@@@
+//#define USE_NETWORK // 주석하면 서버없이 가능@@@@@@@@@
 
 // =================================================
 
