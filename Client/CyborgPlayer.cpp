0b998844a03e8c59b730c215c3b70a68c4523717
Sang Min <133941406+Olsangmin@users.noreply.github.com>
2024-04-16 22:11:20 +0900

    Player Container test

--- Client/CyborgPlayer.cpp
+++ Client/CyborgPlayer.cpp
@@ -84,7 +84,7 @@ CCamera* CyborgPlayer::ChangeCamera(DWORD nNewCameraMode, float fTimeElapsed)
 	case THIRD_PERSON_CAMERA:
 		SetFriction(130.0f);
 		SetGravity(XMFLOAT3(0.0f, -250.0f, 0.0f));
-		SetMaxVelocityXZ(40.0f);
+		SetMaxVelocityXZ(20.0f);
 		SetMaxVelocityY(400.0f);
 		m_pCamera = OnChangeCamera(THIRD_PERSON_CAMERA, nCurrentCameraMode);
 		m_pCamera->SetTimeLag(0.05f);
