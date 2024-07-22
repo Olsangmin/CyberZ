3f9b3140e351fed207745647a0895db46ad01808
Olsangmin <133941406+Olsangmin@users.noreply.github.com>
2024-07-22 23:21:26 +0900

    Modified IPadress

--- Client/GameFramework.cpp
+++ Client/GameFramework.cpp
@@ -443,8 +443,8 @@ void CGameFramework::BuildObjects(int myPlayerNum)
 
 	// Make Scene
 	// Ã³À½ ¾À ºôµå
-	m_nSceneNum = SECOND_ROUND_SCENE;
-	m_pScene = new CSecondRoundScene();
+	m_nSceneNum = START_SCENE;
+	m_pScene = new CStartScene();
 	if (m_pScene) m_pScene->BuildObjects(m_pd3dDevice, m_pd3dCommandList, 0);
 	if(m_pScene) m_pScene->CreateShadowShader(m_pd3dDevice, m_pd3dCommandList);
 
--- Client/Network.cpp
+++ Client/Network.cpp
@@ -1,6 +1,6 @@
 #include "Network.h"
 
-string ServerIP = { "127.0.0.1" };
+string ServerIP = { "49.173.80.42" };
 
 string Login_Id = { "Client ID" };
 string Login_PassWord = { "0000" };
